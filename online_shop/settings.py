e3ec78e08cfae7b3e61a78e5d416e86989dd8fa8
agzet <agzetjik@gmail.com>
2026-01-28 08:13:04 +0600

    wilder

--- online_shop/settings.py
+++ online_shop/settings.py
@@ -131,6 +131,7 @@ MODELTRANSLATION_LANGUAGES = ('ru', 'en')
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = 'media/'
 
